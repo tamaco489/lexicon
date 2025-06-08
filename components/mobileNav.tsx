83518dad274381ac701ddeb9cac548a74988294c
tamaco489 <tamaco489@gmail.com>
2025-06-08 17:04:02 +0900

    add: useLockBodyScroll

--- components/mobileNav.tsx
+++ components/mobileNav.tsx
@@ -1,12 +1,17 @@
 import { siteConfig } from "@/config/site";
 import { NavItem } from "@/types";
 import Link from "next/link";
+import { useLockBodyScroll } from "@uidotdev/usehooks";
 
 interface MobileNavProps {
   items: NavItem[];
 }
 
 export default function MobileNav({ items }: MobileNavProps) {
+
+  // モバイルナビゲーションが開いている時に、背景のスクロールを防止するためのフック
+  useLockBodyScroll();
+
   return (
     <div className="fixed top-16 inset-0 z-50 p-6 shadow-md md:hidden animate-in slide-in-from-bottom-80">
       <div className="grid gap-6 bg-popover p-4 text-popover-foreground shadow-md">
