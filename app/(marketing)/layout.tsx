84852beefb2dee8913bc9ea889338ff7fc666eaf
tamaco489 <tamaco489@gmail.com>
2025-06-08 15:05:29 +0900

    add: header layout components

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,33 @@
+import MainNav from "@/components/mainNav";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+export default function MarketingLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      {/* header */}
+      <header className="container z-40 bg-background">
+        <div className="h-20 py-6 flex items-center justify-between">
+          <MainNav />
+          <Link
+            href={"/"}
+            className={cn(
+              buttonVariants({ variant: "secondary", size: "sm" }),
+              "px-4 py-2"
+            )}
+          >
+            ログイン
+          </Link>
+        </div>
+      </header>
+
+      {/* main */}
+      <main className="flex-1">
+        {children}
+      </main>
+
+      {/* footer */}
+    </>
+  );
+};
---
+++ components/mainNav.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+export default function MainNav() {
+  return (
+    <div className="flex items-center md:gap-10 gap-2">
+      <Link
+        href={"/"}
+        className="hidden md:flex items-center space-x-2"
+      >
+        <span className="font-bold hidden sm:inline-block">Post Writer</span>
+      </Link>
+
+      <nav className="md:flex gap-6 hidden">
+        {/* blog */}
+        <Link
+          href={"/blog"}
+          className="text-lg sm:text-sm font-medium hover:text-foreground/80"
+        >
+          ブログ
+        </Link>
+
+        {/* feature */}
+        <Link
+          href={"#feature"}
+          className="text-lg sm:text-sm font-medium hover:text-foreground/80"
+        >
+          特徴
+        </Link>
+      </nav>
+    </div>
+  );
+};
