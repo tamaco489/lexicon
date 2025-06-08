8466e56dc85270ab836c91652db230669fd4a360
tamaco489 <tamaco489@gmail.com>
2025-06-08 12:21:39 +0900

    fix: route group

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,9 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default function IndexPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen pt-20">
-      <h1 className="text-4xl font-bold">Hello World</h1>
+    <div>
+      <h1>Marketing Page</h1>
       <Button>Click me</Button>
       <Button variant="outline">Click me</Button>
       <Button variant="secondary">Click me</Button>
