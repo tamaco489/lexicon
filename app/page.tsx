96e0d8ea402b320e33fa28b59c34b7af9e3d22ea
tamaco489 <tamaco489@gmail.com>
2025-06-08 12:15:09 +0900

    add: shadcn-ui component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen pt-20">
+      <h1 className="text-4xl font-bold">Hello World</h1>
+      <Button>Click me</Button>
+      <Button variant="outline">Click me</Button>
+      <Button variant="secondary">Click me</Button>
+      <Button variant="destructive">Click me</Button>
+      <Button variant="ghost">Click me</Button>
+      <Button variant="link">Click me</Button>
     </div>
   );
 }
