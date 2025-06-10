c14d0281d0439e222a9118805255bc8fc7d431b2
tamaco489 <tamaco489@gmail.com>
2025-06-10 15:19:19 +0900

    fix: content layer config (#7)

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -2,15 +2,41 @@ import { defineDocumentType, makeSource } from 'contentlayer/source-files'
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
-  filePathPattern: `**/*.md`,
-  contentType: 'markdown',
+  filePathPattern: `blog/**/*.md`,
   fields: {
-    title: { type: 'string', required: true },
-    date: { type: 'date', required: true },
+    title: {
+      type: 'string',
+      required: true,
+    },
+    description: {
+      type: 'string',
+      required: true,
+    },
+    date: {
+      type: 'date',
+      required: true,
+    },
+    published: {
+      type: 'boolean',
+      required: true,
+      default: true,
+    },
+    image: {
+      type: 'string',
+      required: false,
+    },
   },
-}))
+  computedFields: {
+    slug: {
+      type: 'string',
+      resolve: (doc) => `${doc._raw.flattenedPath}`,
+    },
+  },
+}));
 
-export default makeSource({
-  contentDirPath: 'content',
+const config = makeSource({
+  contentDirPath: './content',
   documentTypes: [Post],
-})
+});
+
+export default config;
